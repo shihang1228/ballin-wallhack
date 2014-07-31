a591fcbc310f92b40886cd6f7fb636c8a486ff23
shihang <study1228.sh@gmail.com>
2014-07-31 15:42:27 +0800

    分别将测试案例提取成Test方法

--- src/test/java/com/baldurtech/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/FizzBuzzTest.java
@@ -3,12 +3,36 @@ package com.baldurtech;
 
 public class FizzBuzzTest
 {
+	
 	public static void main(String[] args)
+	{		
+		test_1();
+		test_2();
+		test_3();
+		test_4();	
+	}
+	
+	public static void test_1()
 	{
 		FizzBuzz fizzBuzz = new FizzBuzz();
 		System.out.println("1".equals(fizzBuzz.say(1)));
+	}
+	
+	public static void test_2()
+	{
+		FizzBuzz fizzBuzz = new FizzBuzz();
 		System.out.println("2".equals(fizzBuzz.say(2)));
+	}
+	
+	public static void test_3()
+	{
+		FizzBuzz fizzBuzz = new FizzBuzz();
 		System.out.println("Fizz".equals(fizzBuzz.say(3)));
+	}
+	
+	public static void test_4()
+	{
+		FizzBuzz fizzBuzz = new FizzBuzz();
 		System.out.println("Buzz".equals(fizzBuzz.say(5)));
 	}
 }
