2d12d22e915d0ac4cdd692bad48239d043be1733
shihang <study1228.sh@gmail.com>
2014-07-31 16:05:37 +0800

    将打印结果抽取成方法并且调用

--- src/test/java/com/baldurtech/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/FizzBuzzTest.java
@@ -7,6 +7,7 @@ public class FizzBuzzTest
 	public static void main(String[] args)
 	{
 		FizzBuzzTest fizzBuzzTest = new FizzBuzzTest();
+		
 		fizzBuzzTest.test_1();
 		fizzBuzzTest.test_2();
 		fizzBuzzTest.test_3();
@@ -15,26 +16,31 @@ public class FizzBuzzTest
 	
 	public void test_1()
 	{		
-		System.out.println(assertEquals("1", 1));
+		outputTestReport(assertEquals("1", 1));
+		
 	}
 	
 	public void test_2()
 	{
-		System.out.println(assertEquals("2", 2));
+		outputTestReport(assertEquals("2", 2));
 	}
 	
 	public void test_3()
 	{
-		System.out.println(assertEquals("Fizz", 3));
+		outputTestReport(assertEquals("Fizz", 3));
 	}
 	
 	public void test_4()
 	{
-		System.out.println(assertEquals("Buzz", 5));
+		outputTestReport(assertEquals("Buzz", 5));
 	}
 	
 	public Boolean assertEquals(String expectedResult, int param)
 	{
 		return expectedResult.equals(fizzBuzz.say(param));
+	}
+	public void outputTestReport(boolean report)
+	{
+		System.out.println(report);
 	}
 }
