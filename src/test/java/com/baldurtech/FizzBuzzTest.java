8e0ee664478f888c81b07757c9968e9e18c5403b
shihang <study1228.sh@gmail.com>
2014-07-31 15:47:27 +0800

    通过FizzBuzzTest对象调用Test方法

--- src/test/java/com/baldurtech/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/FizzBuzzTest.java
@@ -3,36 +3,33 @@ package com.baldurtech;
 
 public class FizzBuzzTest
 {
-	
+	FizzBuzz fizzBuzz = new FizzBuzz();
 	public static void main(String[] args)
-	{		
-		test_1();
-		test_2();
-		test_3();
-		test_4();	
+	{
+		FizzBuzzTest fizzBuzzTest = new FizzBuzzTest();
+		fizzBuzzTest.test_1();
+		fizzBuzzTest.test_2();
+		fizzBuzzTest.test_3();
+		fizzBuzzTest.test_4();	
 	}
 	
-	public static void test_1()
-	{
-		FizzBuzz fizzBuzz = new FizzBuzz();
+	public void test_1()
+	{		
 		System.out.println("1".equals(fizzBuzz.say(1)));
 	}
 	
-	public static void test_2()
+	public void test_2()
 	{
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		System.out.println("2".equals(fizzBuzz.say(2)));
 	}
 	
-	public static void test_3()
+	public void test_3()
 	{
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		System.out.println("Fizz".equals(fizzBuzz.say(3)));
 	}
 	
-	public static void test_4()
+	public void test_4()
 	{
-		FizzBuzz fizzBuzz = new FizzBuzz();
 		System.out.println("Buzz".equals(fizzBuzz.say(5)));
 	}
 }
