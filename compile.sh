292924ef8fdd1c929fb600145c5f22a0e7538714
shihang <study1228.sh@gmail.com>
2014-07-31 14:51:10 +0800

    添加清空，编译，运行脚本

---
+++ compile.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir -p target/classes 
+
+javac -d target/classes src/main/java/com/baldurtech/FizzBuzz.java
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -classpath target/classes com.baldurtech.FizzBuzz
