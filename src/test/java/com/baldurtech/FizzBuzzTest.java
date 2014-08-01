d897933dba56b62dcb5aa7eedd1bf8e8ce33201b
shihang <study1228.sh@gmail.com>
2014-08-01 10:09:19 +0800

    将实际结果用方法来传递

--- src/test/java/com/baldurtech/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/FizzBuzzTest.java
@@ -9,59 +9,59 @@ public class FizzBuzzTest
 	public static void main(String[] args)
 	{
 		FizzBuzzTest fizzBuzzTest = new FizzBuzzTest();
-		
 		fizzBuzzTest.test_1();
 		fizzBuzzTest.test_2();
 		fizzBuzzTest.test_3();
-		fizzBuzzTest.test_4();	
+		fizzBuzzTest.test_4();
 		fizzBuzzTest.test_5();
 		fizzBuzzTest.test_6();
 		fizzBuzzTest.test_7();
 		
 		fizzBuzzTest.outputTestReport();
+		
 	}
 	
 	public void test_1()
-	{	
-		assertEquals("1", 1);
+	{
+		assertEquals("1", fizzBuzz.say(1));
 	}
+	
 	
 	public void test_2()
 	{
-		assertEquals("2", 2);	
+		assertEquals("2", fizzBuzz.say(2));	
 	}
 	
 	public void test_3()
 	{
-		assertEquals("Fizz", 3);	
+		assertEquals("Fizz", fizzBuzz.say(3));	
 	}
 	
 	public void test_4()
 	{
-		assertEquals("Buzz", 5);	
+		assertEquals("Buzz", fizzBuzz.say(5));	
 	}
 	
 	public void test_5()
 	{
-		assertEquals("Buzz",10);
+		assertEquals("Buzz",fizzBuzz.say(10));
 	}
 	
 	public void test_6()
 	{
-		assertEquals("Fizz",6);
+		assertEquals("Fizz",fizzBuzz.say(6));
 	}
 	public void test_7()
 	{
-		assertEquals("FizzBuzz",15);
+		assertEquals("FizzBuzz",fizzBuzz.say(15));
 	}
 	
-	public void assertEquals(String expectedResult, int param)
+	public void assertEquals(String expectedResult, String actualResult)
 	{
-		String actualResult = fizzBuzz.say(param);
-		if(false == actualResult.equals(expectedResult))
+		if(false == expectedResult.equals(actualResult))
 		{
 			result = false;
-			System.out.println("Expected FizzBuzz, But " + fizzBuzz.say(15));
+			System.out.println("Expected: " + expectedResult + " ,but: " + actualResult);
 		}
 	}
 	public void outputTestReport()
