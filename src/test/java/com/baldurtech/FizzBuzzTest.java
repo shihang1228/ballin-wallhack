31f769bdf4587cf4906aae8ac35e93c176d5098e
shihang <study1228.sh@gmail.com>
2014-08-01 10:15:27 +0800

    让测试案例互相独立，结果不相互影响

--- src/test/java/com/baldurtech/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/FizzBuzzTest.java
@@ -3,7 +3,6 @@ package com.baldurtech;
 
 public class FizzBuzzTest
 {
-	FizzBuzz fizzBuzz = new FizzBuzz();
 	public static Boolean result = true;
 	
 	public static void main(String[] args)
@@ -23,36 +22,43 @@ public class FizzBuzzTest
 	
 	public void test_1()
 	{
+		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("1", fizzBuzz.say(1));
 	}
 	
 	
 	public void test_2()
 	{
+		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("2", fizzBuzz.say(2));	
 	}
 	
 	public void test_3()
 	{
+		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("Fizz", fizzBuzz.say(3));	
 	}
 	
 	public void test_4()
 	{
+		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("Buzz", fizzBuzz.say(5));	
 	}
 	
 	public void test_5()
 	{
+		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("Buzz",fizzBuzz.say(10));
 	}
 	
 	public void test_6()
 	{
+		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("Fizz",fizzBuzz.say(6));
 	}
 	public void test_7()
 	{
+		FizzBuzz fizzBuzz = new FizzBuzz();
 		assertEquals("FizzBuzz",fizzBuzz.say(15));
 	}
 	
