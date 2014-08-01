639b33a3bf9c8e89d9fdae35c6a6339751021c11
shihang <study1228.sh@gmail.com>
2014-08-01 10:32:47 +0800

    修改测试案例的方法名，提升测试代码的可读性

--- src/test/java/com/baldurtech/FizzBuzzTest.java
+++ src/test/java/com/baldurtech/FizzBuzzTest.java
@@ -8,58 +8,17 @@ public class FizzBuzzTest
 	public static void main(String[] args)
 	{
 		FizzBuzzTest fizzBuzzTest = new FizzBuzzTest();
-		fizzBuzzTest.test_1();
-		fizzBuzzTest.test_2();
-		fizzBuzzTest.test_3();
-		fizzBuzzTest.test_4();
-		fizzBuzzTest.test_5();
-		fizzBuzzTest.test_6();
-		fizzBuzzTest.test_7();
+		FizzBuzzTestCase fizzBuzzTestCase = new FizzBuzzTestCase();
+		fizzBuzzTestCase.test_1_should_be_1();
+		fizzBuzzTestCase.test_2_should_be_2();
+		fizzBuzzTestCase.test_3_should_be_Fizz();
+		fizzBuzzTestCase.test_5_should_be_Buzz();
+		fizzBuzzTestCase.test_6_should_be_Fizz();
+		fizzBuzzTestCase.test_10_should_be_Buzz();
+		fizzBuzzTestCase.test_15_should_be_FizzBuzz();
 		
 		fizzBuzzTest.outputTestReport();
 		
-	}
-	
-	public void test_1()
-	{
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		assertEquals("1", fizzBuzz.say(1));
-	}
-	
-	
-	public void test_2()
-	{
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		assertEquals("2", fizzBuzz.say(2));	
-	}
-	
-	public void test_3()
-	{
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		assertEquals("Fizz", fizzBuzz.say(3));	
-	}
-	
-	public void test_4()
-	{
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		assertEquals("Buzz", fizzBuzz.say(5));	
-	}
-	
-	public void test_5()
-	{
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		assertEquals("Buzz",fizzBuzz.say(10));
-	}
-	
-	public void test_6()
-	{
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		assertEquals("Fizz",fizzBuzz.say(6));
-	}
-	public void test_7()
-	{
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		assertEquals("FizzBuzz",fizzBuzz.say(15));
 	}
 	
 	public void assertEquals(String expectedResult, String actualResult)
