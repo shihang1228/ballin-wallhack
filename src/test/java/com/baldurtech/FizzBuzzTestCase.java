13f90590fac90ba3ba7d0b916cbe5eb73cfc5179
shihang <study1228.sh@gmail.com>
2014-08-01 15:30:34 +0800

    通过继承FizzBuzzTest直接使用assertEquals方法

--- src/test/java/com/baldurtech/FizzBuzzTestCase.java
+++ src/test/java/com/baldurtech/FizzBuzzTestCase.java
@@ -1,47 +1,46 @@
 package com.baldurtech;
 
-public class FizzBuzzTestCase
+public class FizzBuzzTestCase extends FizzBuzzTest
 {
-	FizzBuzzTest fizzBuzzTest = new FizzBuzzTest();
 	public void test_1_should_be_1()
 	{
 		FizzBuzz fizzBuzz = new FizzBuzz();
-		fizzBuzzTest.assertEquals("1", fizzBuzz.say(1));
+		assertEquals("1", fizzBuzz.say(1));
 	}
 	
 	public void test_2_should_be_2()
 	{
 		FizzBuzz fizzBuzz = new FizzBuzz();
-		fizzBuzzTest.assertEquals("2", fizzBuzz.say(2));	
+		assertEquals("2", fizzBuzz.say(2));	
 	}
 	
 	public void test_3_should_be_Fizz()
 	{
 		FizzBuzz fizzBuzz = new FizzBuzz();
-		fizzBuzzTest.assertEquals("Fizz", fizzBuzz.say(3));	
+		assertEquals("Fizz", fizzBuzz.say(3));	
 	}
 	
 	public void test_5_should_be_Buzz()
 	{
 		FizzBuzz fizzBuzz = new FizzBuzz();
-		fizzBuzzTest.assertEquals("Buzz", fizzBuzz.say(5));	
+		assertEquals("Buzz", fizzBuzz.say(5));	
 	}
 	
 	public void test_6_should_be_Fizz()
 	{
 		FizzBuzz fizzBuzz = new FizzBuzz();
-		fizzBuzzTest.assertEquals("Fizz",fizzBuzz.say(6));
+		assertEquals("Fizz",fizzBuzz.say(6));
 	}
 	
 	public void test_10_should_be_Buzz()
 	{
 		FizzBuzz fizzBuzz = new FizzBuzz();
-		fizzBuzzTest.assertEquals("Buzz",fizzBuzz.say(10));
+		assertEquals("Buzz",fizzBuzz.say(10));
 	}
 	
 	public void test_15_should_be_FizzBuzz()
 	{
 		FizzBuzz fizzBuzz = new FizzBuzz();
-		fizzBuzzTest.assertEquals("FizzBuzz",fizzBuzz.say(15));
+		assertEquals("FizzBuzz",fizzBuzz.say(15));
 	}
 }
